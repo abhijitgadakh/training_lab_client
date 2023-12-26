3f2350ed7ae75b1f41bf2d8086a301f6b3395472
Abhijit Gadakh <gadakhabhijit@gmail.com>
2023-12-26 23:01:54 +0530

    #002 - initial angular component Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/shared/login/login.component';
+import { LogoutComponent } from './modules/shared/logout/logout.component';
+import { AdminHomeComponent } from './modules/admin/admin-home/admin-home.component';
+import { LearnerHomeComponent } from './modules/learner/learner-home/learner-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    AdminHomeComponent,
+    LearnerHomeComponent
   ],
   imports: [
     BrowserModule,
